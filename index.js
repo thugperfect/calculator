1cb98d738938d75c5bde5ccf185a7c5771dc1344
Subramanian <102452460+thugperfect@users.noreply.github.com>
2023-02-13 04:23:15 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+function one(){
+   formm.answer.value+="1"
+
+}
+function two(){
+    formm.answer.value+="2"
+ 
+ }
+ function three(){
+    formm.answer.value+="3"
+ }
+ function four(){
+    formm.answer.value+="4"
+ }
+ function five(){
+    formm.answer.value+="5"
+ 
+ }
+ function six(){
+    formm.answer.value+="6"
+ }
+ function seven(){
+    formm.answer.value+="7"
+ 
+ }
+ function eight(){
+    formm.answer.value+="8"
+ 
+ }
+ function nine(){
+    formm.answer.value+="9"
+ 
+ }
+ function zero(){
+    formm.answer.value+="0"
+ 
+ }
+ function decimal(){
+    formm.answer.value+="."
+ 
+ }
+ function add(){
+    formm.answer.value+="+"
+    
+ 
+ }
+ function sub(){
+    formm.answer.value+="-"
+ 
+ }
+ function mul(){
+    formm.answer.value+="*"
+ }
+ function div(){
+    formm.answer.value+="/"
+ 
+ }
+ function equals(){
+    formm.answer.value=eval(formm.answer.value)
+     
+ 
+ }
+ function clr() {
+    formm.answer.value=""
+ 
+ }
+ function percentage(){
+     formm.answer.value+="%"
+ }
+ function sq(){
+    formm.answer.value=eval(formm.answer.value*formm.answer.value)
+ }
+ function type(){
+    formm.answer.value+=KeyboardEvent
+
+ }
