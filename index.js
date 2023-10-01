05cb5b36e50fb9d5e4327c87d28c3c5f9f552d8a
thugperfect <meenamani9444@gmail.com>
2023-10-01 15:44:43 +0530

    btns clicked and display

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const input = document.getElementById('input')
-
+const inputField = document.getElementById('inp')
 const btnsTop = ['√','<','C']
 
-const btnsMiddle = ['9','8','7','6','5','4','3','2','1','0']
+const btnsMiddle = ['1','2','3','4','5','6','7','8','9','0']
 
 const btnsBottom = ['+','-','*','/','^']
 
@@ -11,9 +11,9 @@ function createBtns (){
    for(i = 0;i<btnsTop.length;i++){
       const btnTop = document.createElement('div')
       btnTop.className = 'btns'
-      btnTop.id = i+15
+      btnTop.id = i+16
       btnTop.innerText = btnsTop[i]
-      if(btnTop.id === '17'){
+      if(btnTop.id === '18'){
          btnTop.style.color = 'red'
       }
       input.appendChild(btnTop)
@@ -22,19 +22,31 @@ function createBtns (){
    for(i = 0;i<btnsMiddle.length;i++){
       const btnMiddle = document.createElement('div')
       btnMiddle.className = 'btns'
-      btnsTop.id = i+1
+      btnMiddle.id = i+1
       btnMiddle.innerText = btnsMiddle[i]
       input.appendChild(btnMiddle)
    }
    for (i = 0;i<btnsBottom.length;i++){
       const btnBottom = document.createElement('div')
       btnBottom.className = 'btns'
-      btnBottom.id = i+10
+      btnBottom.id = i+11
       btnBottom.innerText = btnsBottom[i]
       input.appendChild(btnBottom)
    }
 }
+function btnClick(){
+   const btns = document.querySelectorAll('.btns')
+   for(i =0;i<btns.length;i++){
+      btns[i].addEventListener('click',clicked)
+   }
+}
+function clicked(e){
+const clickedBtn = Number(e.target.id)
+console.log(clickedBtn)
+inputField.value += clickedBtn
 
 
+}
 
 createBtns()
+btnClick()
